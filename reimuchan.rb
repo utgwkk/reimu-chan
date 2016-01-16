2749cef86e19c3c16d8fea40485ff81c176a33d0
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-01-16 15:46:37 +0900

    Create basis

---
+++ reimuchan.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+#require 'slack'
+require 'twitter'
+
+YOUR_SCREEN_NAME = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token        = ENV['TWITTER_ACCESS_TOKEN_KEY']
+  config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
+end.verify_credentials.screen_name
+
+client = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token        = ENV['TWITTER_ACCESS_TOKEN_KEY']
+  config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
+end
+
+Slack.configure do |config|
+  config.token = ENV['SLACK_TOKEN']
+end
+
+client.user do |data|
+end
